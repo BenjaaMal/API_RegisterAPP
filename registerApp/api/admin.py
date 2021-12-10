dccaeb4b1ba7035f41bfb853145eab58447d3d40
BenjaaMal <89644738+BenjaaMal@users.noreply.github.com>
2021-12-10 16:38:00 -0300

    Delete registerApp/api directory

--- registerApp/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Asignatura, Asistencia, Alumno, Profesor, Persona
-
-# Register your models here.
-admin.site.register(Asistencia)
-admin.site.register(Asignatura)
-admin.site.register(Alumno)
-admin.site.register(Profesor)
-admin.site.register(Persona)
